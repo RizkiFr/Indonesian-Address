acc73f877d96199461128fdf1934f88559acf0ce
Rizki Fr <rizqifauzi12@gmail.com>
2020-02-09 03:17:17 +0700

    Februari 2020

---
+++ scripts/mdf_parser.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python2
+
+import csv
+import sys
+import xml.etree.cElementTree as ET
+
+provinces_dict = {}
+regencies_dict = {}
+districts_dict = {}
+villages_dict = {}
+
+
+def read_html_data(fname):
+    print 'Reading html data...'
+    with open(fname, 'rb') as inputfile:
+        append = False
+        for line in inputfile:
+            if '<tr class="table_content">' in line:
+                inputbuffer = line
+                append = True
+            elif '</tr>' in line:
+                if append:
+                    inputbuffer += line
+                    append = False
+                    process_buffer(inputbuffer)
+                    inputbuffer = ''
+            elif append:
+                inputbuffer += line
+
+
+def process_buffer(buf):
+    tnode = ET.fromstring(buf)
+    # print tnode[0].text.strip()
+    province_id = tnode[1].text.strip()
+    province_name = tnode[2].text.strip()
+    regency_id = province_id + tnode[3].text.strip()
+    regency_name = tnode[4].text.strip()
+    district_id = regency_id + tnode[5].text.strip()
+    district_name = tnode[6].text.strip()
+    village_id = district_id + tnode[7].text.strip()
+    village_name = tnode[8].text.strip()
+
+    provinces_dict[province_id] = province_name
+    regencies_dict[regency_id] = regency_name
+    districts_dict[district_id] = district_name
+    villages_dict[village_id] = village_name
+
+
+def write_data_to_csv(tmp_dir, key):
+    print 'Writing provinces data...'
+    write_dict_to_csv(tmp_dir + '/provinces-' + key + '.csv', provinces_dict)
+    print 'Writing regencies data...'
+    write_dict_to_csv(tmp_dir + '/regencies-' + key + '.csv', regencies_dict, 2)
+    print 'Writing districts data...'
+    write_dict_to_csv(tmp_dir + '/districts-' + key + '.csv', districts_dict, 4)
+    print 'Writing villages data...'
+    write_dict_to_csv(tmp_dir + '/villages-' + key + '.csv', villages_dict, 7)
+    print 'Done.'
+
+
+def write_dict_to_csv(fname, data_dict, upper_level_key_length=0):
+    with open(fname, 'wb') as fp:
+        fcsv = csv.writer(fp, delimiter=',')
+        if upper_level_key_length > 0:
+            for key, value in sorted(data_dict.iteritems()):
+                fcsv.writerow([
+                    key.encode('utf-8'),
+                    key[:upper_level_key_length].encode('utf-8'),
+                    value.encode('utf-8')])
+        else:
+            for key, value in sorted(data_dict.iteritems()):
+                fcsv.writerow([key, value])
+
+
+def main(argv):
+    if (len(argv) > 0):
+        read_html_data(argv[0] + '/' + argv[1])
+        write_data_to_csv(argv[0], argv[2])
+    else:
+        print "usage: mdf_parser.py <directory> <html_input_file> <key>"
+        sys.exit(2)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ scripts/templates/mysql_header.sql
@@ -0,0 +1,76 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `provinces`
+--
+
+DROP TABLE IF EXISTS `provinces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS `provinces` (
+  `id` char(2) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `regencies`
+--
+
+DROP TABLE IF EXISTS `regencies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS `regencies` (
+  `id` char(4) COLLATE utf8_unicode_ci NOT NULL,
+  `province_id` char(2) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `regencies_province_id_index` (`province_id`),
+  CONSTRAINT `regencies_province_id_foreign` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `districts`
+--
+
+DROP TABLE IF EXISTS `districts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS `districts` (
+  `id` char(7) COLLATE utf8_unicode_ci NOT NULL,
+  `regency_id` char(4) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `districts_id_index` (`regency_id`),
+  CONSTRAINT `districts_regency_id_foreign` FOREIGN KEY (`regency_id`) REFERENCES `regencies` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `villages`
+--
+
+DROP TABLE IF EXISTS `villages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS `villages` (
+  `id` char(10) COLLATE utf8_unicode_ci NOT NULL,
+  `district_id` char(7) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `villages_district_id_index` (`district_id`),
+  CONSTRAINT `villages_district_id_foreign` FOREIGN KEY (`district_id`) REFERENCES `districts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
